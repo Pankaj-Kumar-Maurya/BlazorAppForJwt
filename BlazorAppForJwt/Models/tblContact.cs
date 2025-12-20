768b1d8f1e413b72e164a399e90aaf49b8c30f62
Pankaj Kumar Maurya <pankajmaurya872691@gmail.com>
2025-12-20 15:50:52 +0530

    Add project files.

---
+++ BlazorAppForJwt/Models/tblContact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIForJwt.Models
+{
+    public class tblContact
+    {
+        [Key]
+        public int ContactID { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+        public string? Email { get; set; }
+        public DateTime DOB { get; set; }
+        public string? ContactNo { get; set; }
+        
+    }
+}
